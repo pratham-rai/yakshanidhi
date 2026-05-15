75d078f18311c8ad3950ec8c49f8cec19bdc6ce2
Pratham Rai <pratham@example.com>
2026-05-16 04:52:32 +0530

    fix: force build trigger v2

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config(); // Trigger build v2
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
